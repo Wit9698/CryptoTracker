720b1c0dbb664457c7bc42800bc1731b2f1c7065
Witold <gasowski.witold@gmail.com>
2022-03-14 14:31:48 +0100

    fix registration problems

--- src/main/java/com/learning/cryptotracker/registration/RegistrationController.java
+++ src/main/java/com/learning/cryptotracker/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.learning.cryptotracker.registration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RegistrationController {
 
-    private RegistrationService registrationService;
+    private final RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
