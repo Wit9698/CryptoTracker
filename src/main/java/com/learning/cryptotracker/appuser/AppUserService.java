d6e387e21140ea0e5b6927d83dbb851fbe51bc2c
Witold <gasowski.witold@gmail.com>
2022-03-14 15:08:27 +0100

    sign up method

--- src/main/java/com/learning/cryptotracker/appuser/AppUserService.java
+++ src/main/java/com/learning/cryptotracker/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +13,24 @@ public class AppUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     private final AppUserRepo appUserRepo;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepo.findByEmail(email).orElseThrow(() ->
                 new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser){
+        boolean userExist = appUserRepo.findByEmail(appUser.getEmail()).isPresent();
+
+        if(userExist){
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        appUserRepo.save(appUser);
+        return "works";
     }
 }
--- src/main/java/com/learning/cryptotracker/registration/RegistrationService.java
+++ src/main/java/com/learning/cryptotracker/registration/RegistrationService.java
@@ -1,11 +1,31 @@
 package com.learning.cryptotracker.registration;
 
+import com.learning.cryptotracker.appuser.AppUser;
+import com.learning.cryptotracker.appuser.AppUserRole;
+import com.learning.cryptotracker.appuser.AppUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RegistrationService {
 
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
